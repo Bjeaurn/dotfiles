b0603acb7297a91919b4d628e73633b8dbbd48d9
Bjorn 'Bjeaurn <bjorn@somnu.com>
2019-04-09 11:44:54 +0200

    Adds a better git-fzf, adds a binding to ctrl+g for it.

--- zsh/scripts/environment/git-fzf.sh
+++ zsh/scripts/environment/git-fzf.sh
@@ -1,13 +1,16 @@
 function select_branch() {
-  local branch=$(git branch -a --list --format "%(refname:lstrip=2)" | fzf)
+  local branch
+  
+  zle -M "Select a branch to switch to:\n"
+
+  branch=$(git branch -a --list --format "%(refname:lstrip=2)" | FZF_DEFAULT_OPTS="--height 30% $FZF_DEFAULT_OPTS -n2..,.. --tiebreak=index --bind=ctrl-r:toggle-sort $FZF_CTRL_R_OPTS --query=${(qqq)LBUFFER} +m" fzf)
 
   if [[ ! -z "$branch" ]];
   then
+    # Note to future self: the syntax ${variable/<regex>/<replacement>} is just frikkin awesome
     git checkout ${branch/origin\//}
   fi
 
-  echo "\n"
-
+  echo "\n\n"
   zle reset-prompt
 }
-
