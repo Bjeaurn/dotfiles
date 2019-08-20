54cff10055b9f4b6a664f5fc982ffbb397c7a26b
Bjorn 'Bjeaurn <bjorn@somnu.com>
2019-08-20 18:31:15 +0200

    feat(gsc): Updates the single letter aliases.

--- git/semantic-commits.sh
+++ git/semantic-commits.sh
@@ -9,17 +9,10 @@ function register_git_alias() {
     if [[ -z $2 ]]; then
       git config --global alias.$1 '!f() { [[ -z "$GIT_PREFIX" ]] || cd "$GIT_PREFIX" && if [ "$#" == 0 ]; then git commit -m "'$1': " --edit; elif [ "$#" == 2 ]; then git commit -m "'$1'(${1}): ${@:2}"; else git commit -m "'$1': ${@}"; fi }; f'
     else
-      git config --global alias.$1 !git $2
+      echo "Alias $1 to: '$2'"
+      git config --global alias.$1 "!git $2"
     fi
   fi
-}
-
-# Check if command was finished successfully
-#
-# $1 — any shell command
-function command_succes() {
-  command $1 >/dev/null 2>&1 && return 0 # true
-  return 1                               # false
 }
 
 echo 'Installing git aliases…'
@@ -32,19 +25,17 @@ done
 
 # git-extras chore/refactor compatibility (https://github.com/tj/git-extras)
 # Docs: https://github.com/tj/git-extras/blob/master/Commands.md#git-featurerefactorbugchore
-if command_succes 'git extras'; then
-  register_git_alias 'ch' 'chore'
-  register_git_alias 'rf' 'refactor'
-  register_git_alias 'c' 'chore'
-  register_git_alias 'd' 'docs'
-  register_git_alias 'f' 'feat'
-  register_git_alias 'p' 'perf'
-  register_git_alias 'x' 'fix'
-  register_git_alias 'l' 'localize'
-  register_git_alias 'r' 'refactor'
-  register_git_alias 's' 'style'
-  register_git_alias 't' 'test'
-fi
+register_git_alias 'ch' 'chore'
+register_git_alias 'rf' 'refactor'
+register_git_alias 'c' 'chore'
+register_git_alias 'd' 'docs'
+register_git_alias 'f' 'feat'
+register_git_alias 'p' 'perf'
+register_git_alias 'x' 'fix'
+register_git_alias 'l' 'localize'
+register_git_alias 'r' 'refactor'
+register_git_alias 's' 'style'
+register_git_alias 't' 'test'
 
 echo
 echo 'Done! Now you can use semantic commits.'
